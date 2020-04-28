fdc5bfd915906af0b03c2175703c3d5d189f2e96
Amber Worthy <ambercodes3@gmail.com>
2020-04-27 21:49:07 -0700

    Part 1 and Part 2 completed for Mom's Shopping List

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addItem.addEventListener("submit", function(e){
+   event.preventDefault()
+   var input = document.addItem.title.value
+   console.log(input)
+   var newLi = document.createElement("div")
+   var list = document.getElementById("list").append(newLi)
+   var div = document.createElement("div")
+   
+   div.textContent = input
+   newLi.appendChild(div)
+
+   var eBtn = document.createElement("button")
+   newLi.appendChild(eBtn)
+   eBtn.textContent = "edit"
+   
+   var xBtn = document.createElement("button")
+   newLi.appendChild(xBtn)
+   xBtn.textContent = "X"
+
+   xBtn.addEventListener('click', function() {
+   newLi.innerHTML = " "
+   })
+
+});
+
+/*Extra Credit
+A user will be able to click the "edit" button and see an input box appear
+When the user clicks the "edit" button, a "save" button replace the "edit" button
+The input box will automatically have the value of the list item
+The user can edit and "save" the input box's value
+On save, the input box will disappear, and the new value will appear*/
+
+
+
+
