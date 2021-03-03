b8d6769518f1b924b4e8655ce6c5b99a929503ec
Amber Worthy <ambercodes3@gmail.com>
2021-03-03 12:51:42 -0800

    Thing Finder

---
+++ Module5/thing-finder/routes/shoeRouter.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+const shoeRouter = express.Router()
+const uuid = require('uuid').v4
+
+const shoes = [
+    { brand: "Nike", styles: "Nike Air Max 270", price: 150, _id: uuid() },
+    { brand: "Nike", styles: "Nike Air Huarache", price: 74.99, _id: uuid() }, 
+    { brand: "Adidas", styles: "Adidas Originals NMD R1" , price: 140, _id: uuid() }, 
+    { brand: "PUMA", styles: "PUMA Suede Classic Mono", price: 55, _id: uuid() }
+]
+
+shoeRouter.get("/", (req, res) => {
+    res.send(shoes)
+})
+
+
+shoeRouter.get("/search/brand", (req, res) => {
+    const brand = req.query.brand
+    const filteredShoes = shoes.filter(shoe => shoe.brand === brand)
+    res.send(filteredShoes)
+})
+
+module.exports = shoeRouter
---
+++ Module5/thing-finder/server.js
@@ -0,0 +1,12 @@
+  
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.use("/shoes", require("./routes/shoeRouter.js"))
+
+
+app.listen(7000, () => {
+    console.log("The server is running on Port 7000")
+})
